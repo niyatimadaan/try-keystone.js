338941d7855e8915d630a48d2f4cc7bad1b9d1ed
niyatimadaan <madaanniyati@gmail.com>
2024-04-01 16:47:54 +0530

    postgres setup

--- .keystone/config.js
+++ .keystone/config.js
@@ -184,8 +184,11 @@ var keystone_default = withAuth(
       // we're using sqlite for the fastest startup experience
       //   for more information on what database might be appropriate for you
       //   see https://keystonejs.com/docs/guides/choosing-a-database#title
-      provider: "sqlite",
-      url: "file:./keystone.db"
+      // provider: 'sqlite',
+      // url: 'file:./keystone.db',
+      provider: "postgresql",
+      url: process.env.DATABASE_URL || "postgres://node:node@localhost:5432/trykeystone",
+      idField: { kind: "uuid" }
     },
     lists,
     session,
